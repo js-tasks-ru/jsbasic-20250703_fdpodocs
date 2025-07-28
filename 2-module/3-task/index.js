86cc8de15fba0c91f09e8f1b60947de6bf63a2a2
Oksana Kj <fdpodocs@gmail.com>
2025-07-28 09:32:40 +0300

    2-module added

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,16 @@
 let calculator = {
-  // ваш код
+   sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
+
+  read() {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
+  }
 };
 
-// НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
-window.calculator = calculator; // делает ваш калькулятор доступным глобально
+
---
+++ 7-module/1-task/index.js
@@ -0,0 +1,7 @@
+
+function sum(m, n) {
+ return m + n;
+}
+let result = sum(1, 1);
+console.log( result );
+
